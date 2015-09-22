ea9bab12ba78ab1b92f066733647fa60caea110c
Thomas Hepner <thepner1@uc.cl>
2015-09-22 16:50:18 -0300

    Basic structure and Angular functionality

--- UnitConverter/app.js
+++ UnitConverter/app.js
@@ -1,1 +1,17 @@
 var app = angular.module('unitConverter', []);
+
+app.controller('UnitController', function() {
+  this.units = units;
+});
+
+var units = [
+  {
+    name: 'Length'
+  },
+  {
+    name: 'Mass'
+  },
+  {
+    name: 'Temperature'
+  }
+];
