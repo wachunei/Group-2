e26cee1cd56d7e877eba983aa8b22cbe792284f2
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2015-09-25 23:18:17 -0300

    Include Storage Service

--- UnitConverter/app-converter.js
+++ UnitConverter/app-converter.js
@@ -1,25 +1,79 @@
 (function() {
-  var app = angular.module('unitConverter-converter', []);
+  var app = angular.module('unitConverter-converter', ['unitConverter-storage']);
 
-  app.service('converterService', function() {
-
-    this.evaluate = function(m) {
+  app.service('converterService', ['dataService', function(dataService) {
+    var self = this;
+    self.evaluate = function(m) {
       return function(c) {
         return function(x) {
-          return m*x + c;
+          return m * x + c;
         };
       };
     };
 
-    this.converter = {
-      length: {},
-      temperature: {},
-      weigth: {},
-      area: {},
-      volume: {},
-      time: {}
+    if (dataService.storedData.length > 0) {
+      var converter = {
+        length: {},
+        temperature: {},
+        weigth: {},
+        area: {},
+        volume: {},
+        time: {}
+      };
 
-    };
-  });
+      dataService.storedData.forEach(function(obj)  {
+        var type = obj.type,
+          fUnit = obj.fUnit,
+          sUnit = obj.sUnit,
+          m = obj.slope,
+          c = obj.intercept;
+
+        if (!converter[type][fUnit]) {
+          converter[type][fUnit] = {};
+        }
+
+        if (!converter[type][fUnit][sUnit]) {
+          converter[type][fUnit][sUnit] = {};
+        }
+        converter[type][fUnit][sUnit] = self.evaluate(m)(c);
+      });
+
+      self.converter = converter;
+
+    } else {
+      self.converter = {
+        length: {},
+        temperature: {},
+        weigth: {},
+        area: {},
+        volume: {},
+        time: {}
+      };
+    }
+  }]);
+
+  app.service('dataService', ['storageService',
+    function(storageService, converterService) {
+      var self = this;
+      self.storedData = storageService.getData() ||  [];
+
+      self.newConvertion = function(type, fUnit, sUnit, m, c) {
+        self.storedData.push({
+          type: type,
+          fUnit: fUnit,
+          sUnit: sUnit,
+          slope: m,
+          intercept: c
+        });
+      };
+
+      self.saveData = function() {
+        storageService.saveData(self.storedData);
+      };
+
+
+
+    }
+  ]);
 
 })();
--- UnitConverter/app.js
+++ UnitConverter/app.js
@@ -1,42 +1,54 @@
 (function() {
   var app = angular.module('unitConverter', ['unitConverter-converter']);
 
-  app.controller('UnitController',['converterService', '$log', function(converterService, $log) {
+  /* Main conversion form controller */
+  app.controller('UnitController', ['converterService', '$log',
+    function(converterService, $log) {
 
-    this.c = converterService.converter;
-    this.currentType = "length";
+      this.c = converterService.converter;
+      this.currentType = "length";
   }]);
 
+  /* new Unit convertion controller */
+  app.controller('UnitFormController', ['converterService', 'dataService',
+    function(converterService, dataService) {
+      this.selectedType = "length";
 
-  app.controller('UnitFormController', ['converterService', function(converterService) {
-    this.selectedType = "length";
-    this.addConversion = function() {
-      var type = this.selectedType,
+      this.addConversion = function() {
+        var type = this.selectedType,
           fUnit = this.firstUnit,
           sUnit = this.secondUnit,
           m = this.slope,
           c = this.intercept,
+          m_ = (1.0)/m;
+          c_ = (c/m);
           converter = converterService.converter;
 
-      if(!converter[type][fUnit]) {
-        converter[type][fUnit] = {};
-      }
+        if (!converter[type][fUnit]) {
+          converter[type][fUnit] = {};
+        }
 
-      if(!converter[type][sUnit]) {
-        converter[type][sUnit] = {};
-      }
+        if (!converter[type][sUnit]) {
+          converter[type][sUnit] = {};
+        }
 
-      if(!converter[type][fUnit][sUnit]) {
-        converter[type][fUnit][sUnit] = {};
-      }
+        if (!converter[type][fUnit][sUnit]) {
+          converter[type][fUnit][sUnit] = {};
+        }
 
-      if(!converter[type][sUnit][fUnit]) {
-        converter[type][sUnit][fUnit] = {};
-      }
-      converter[type][fUnit][sUnit] = converterService.evaluate(m)(c);
-      converter[type][sUnit][fUnit] = converterService.evaluate(1.0/m)(c/m);
-      this.firstUnit = this.secondUnit = this.slope = this.intercept = null;
-      console.log(converter);
-    };
-  }]);
+        if (!converter[type][sUnit][fUnit]) {
+          converter[type][sUnit][fUnit] = {};
+        }
+        converter[type][fUnit][sUnit] = converterService.evaluate(m)(c);
+        converter[type][sUnit][fUnit] = converterService.evaluate(m_)(c_);
+
+        dataService.newConvertion(type, fUnit, sUnit, m, c);
+        dataService.newConvertion(type, sUnit, fUnit, m_, c_);
+
+        dataService.saveData();
+
+        this.firstUnit = this.secondUnit = this.slope = this.intercept = null;
+      };
+    }
+  ]);
 })();
